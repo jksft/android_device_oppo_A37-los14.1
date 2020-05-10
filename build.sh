58f88df47d255ae2f1a7ceb380295ffc3d8528e7
jksft <53244108+jksft@users.noreply.github.com>
2020-05-10 12:39:38 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 mkdir build && cd build
-repo init --depth=1 -u git://github.com/LineageOS/android.git -b cm-14.1 && repo sync -q -j8
+repo init --depth=1 -u git://github.com/LineageOS/android.git -b cm-14.1 && repo sync -c -f --no-tags --no-clone-bundle -j`nproc`
